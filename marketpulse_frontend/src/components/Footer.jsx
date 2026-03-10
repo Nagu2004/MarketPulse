846ffd7900ee31ee5f38ee1162244f9da22a0f8b
vijay <21A91A0237@aec.edu.in>
2026-03-10 10:49:22 +0530

    adding the footer page

---
+++ marketpulse_frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// import "../css/footer.css";
+
+function Footer(){
+    return(
+        <div className="footer">
+            <h3>&copy; ALL RIGHTS BELONGS TO VARDHAN JAKKA</h3>
+        </div>
+    );
+}
+export default Footer;
