0f161a35fcc0a1cc50ad5290aa85f1e1e8c36dbb
vijay <21A91A0237@aec.edu.in>
2026-03-11 17:10:47 +0530

    addint the chatbot icon and working to the page

--- marketpulse_frontend/src/components/Main_pg.jsx
+++ marketpulse_frontend/src/components/Main_pg.jsx
@@ -1,19 +1,39 @@
 import History from "./History";
 import Home from "./Home";
 import Moreinfo from "./Moreinfo";
+import Chatbot from "./Chatbot";
 import Fetch from "./Fetch";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import "../style/Main_pg.css";
 
-function Main_pg(){
-    return(
+function Main_pg() {
+
+    const [isChatOpen, setIsChatOpen] = useState(false);
+
+    return (
         <div className="main">
+
             <Routes>
-            <Route path="/home/" element={<Home/>}/>
-            <Route path="/history/" element={<History/>}></Route>
-            <Route path="/fetch/" element={<Fetch/>} />
-            <Route path="/info/" element={<Moreinfo/>} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/fetch" element={<Fetch />} />
+                <Route path="/info" element={<Moreinfo />} />
             </Routes>
+
+            {/* Chat button */}
+            {!isChatOpen && (
+                <button
+                    className="chatbot-btn"
+                    onClick={() => setIsChatOpen(true)}
+                ></button>
+            )}
+
+            {/* Chatbot popup */}
+            {isChatOpen && <Chatbot closeChat={() => setIsChatOpen(false)} />}
+
         </div>
     );
 }
+
 export default Main_pg;
